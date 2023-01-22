f77f14a703d194f506238e75e47ff7669ef34a4e
vijay4145 <svijay4145@gmail.com>
2023-01-22 13:46:52 +0530

    added animation lib and event card component

---
+++ frontend/src/components/EventCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export const EventCard = () => {
+    const imgUrl = "https://cdn4.vectorstock.com/i/1000x1000/69/03/flat-icon-sport-events-vector-9456903.jpg";
+  return (
+    <>
+      <br />
+      <div
+        data-aos="fade-right"
+        className="upcomingEventCard mx-3 flex flex-col md:flex-row gap-6 content-center justify-center bg-white "
+        style={{
+          border: "1px solid #ddd",
+          boxShadow: "0 10px 50px -30px black",
+          borderRadius: "30px",
+        }}
+      >
+        <img
+          src={imgUrl}
+          alt=""
+          className="w-full md:w-[20vw] h-[30vh] object-contain min-h-[150px]"
+        />
+        <div className="flex flex-col w-full md:w-[70vw] py-4 content-center">
+          <h1 className="text-2xl font-semibold text-blue-600">
+            Interview for SPORTEC- Core Team
+          </h1>
+          <p>
+            {" "}
+            The most Awaited EVENT of Terna Engineering college SPORTEC 2023 is
+            here. Students those who all are interested to be a part of our
+            sports core committee from Terna Engineering College, Nerul can fill
+            this google form for interviews rest of the details will be shared
+            soon.
+          </p>
+          <span>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-sans py-2 px-3 rounded drop-shadow-sm">
+              Explore More
+            </button>
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
--- frontend/src/components/Events.js
+++ frontend/src/components/Events.js
@@ -1,28 +1,32 @@
 import React, { useEffect } from 'react'
+import AOS from 'aos';
+import "aos/dist/aos.css";
+import { EventCard } from './EventCard';
 
 export const Events = (props) => {
-  const imgUrl = "https://upload.wikimedia.org/wikipedia/commons/6/68/Britain_Needs_You_at_Once_-_WWI_recruitment_poster_-_Parliamentary_Recruiting_Committee_Poster_No._108.jpg";
+
   useEffect(() => {
     props.setProgress(100);
   }, [])
-  
+
+  AOS.init();
+
   return (
     <>
-    <section id='upcomingEvents' className='mt-3 mx-4'>
-      <h1 className='text-blue-900 font-semibold text-3xl'>Upcoming Events :</h1>
+      <section id='upcomingEvents' className='mt-3 mx-4'>
+        
+        <h1 className='text-blue-900 font-semibold text-3xl'>Upcoming Events :</h1>
+        <EventCard/>
+        <EventCard/>
+        <EventCard/>
+        <EventCard/>
+
+      </section>
+      <section id='pastEvents'>
+      </section>
+
       <br/>
-      <div className='upcomingEventCard mx-3 flex flex-col md:flex-row gap-6 content-center justify-center' style={{border: '2px solid red'}}>
-          <img src={imgUrl} alt="" className='w-full md:w-[20vw] h-[30vh] object-contain min-h-[150px]'/>
-          <div className='flex flex-col w-full md:w-[70vw] justify-center content-center'>
-            <h1 >This is Event Name </h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur deleniti beatae dolorem suscipit aliquid quos culpa excepturi ipsa dolor inventore unde voluptate est exercitationem recusandae a corrupti voluptatibus, illo voluptatem? Perspiciatis dolor nostrum assumenda nesciunt illo molestiae cum odio veritatis consectetur non, necessitatibus quia eius numquam at saepe fuga, distinctio ut sapiente id dolores exercitationem provident minus placeat in. Blanditiis, ad. Quae totam, inventore ea quos earum odit reprehenderit, vitae deleniti ducimus consectetur unde alias consequatur velit dolorum exercitationem asperiores ad iure esse maiores! Perferendis quis iusto assumenda animi, quod autem consequuntur ipsam voluptatum ut quasi, ipsum harum. Officiis, inventore.</p>
-
-          </div>
-      </div>
-    </section>
-    <section id='pastEvents'>
-
-    </section>
+      <br/>
     </>
   )
 }
