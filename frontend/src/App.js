0524d2e103faeb995d06b4fb5789a0112f8f2e8f
vijay4145 <svijay4145@gmail.com>
2023-01-22 22:01:49 +0530

    added login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import {
   Routes,
 } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Login } from "./components/Authentication/Login";
 
 function App() {
   const [progress, setProgress] = useState(0);
@@ -26,6 +27,9 @@ function App() {
         progress={progress}
         onLoaderFinished={() => setProgress(0)}
       />
+      <Routes>
+          <Route exact path="/login" element={<Login/>} />
+          
         <div>
           <div className="sticky top-0 bg-white z-10">
             <Navbar />
@@ -38,6 +42,8 @@ function App() {
             <Route exact path="/events" element={<Events setProgress={setProgress}/>} />
           </Routes>
         </div>
+        
+      </Routes>
       </Router>
     </>
   );
---
+++ frontend/src/components/Authentication/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <>
+    <h1>This is Login components</h1>
+    </>
+  )
+}
