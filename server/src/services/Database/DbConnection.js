4fbe9d31770ea524d433f32b1e4cd5dc0dadd984
vijay4145 <svijay4145@gmail.com>
2023-03-04 00:53:14 +0530

    changed connection to mongodb atlas

--- server/src/services/Database/DbConnection.js
+++ server/src/services/Database/DbConnection.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
 const connectToMongo = async ()=>{
-    await mongoose.connect('mongodb://127.0.0.1:27017/TernaWeb');
+    // await mongoose.connect('mongodb://127.0.0.1:27017/TernaWeb');
+    const uri = "mongodb+srv://svijay4145:r6dYBUcYWoUNn1bP@cluster0.l3hycto.mongodb.net/TernaWeb?retryWrites=true&w=majority";
+    await mongoose.connect(uri);
     console.log('mongoose connected1');
 }
 
 connectToMongo().catch(err => console.log(err));
+
+
 module.exports = connectToMongo;
