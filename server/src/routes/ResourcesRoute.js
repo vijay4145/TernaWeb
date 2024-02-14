bf50fadbbe5ac1a9b7afb610191366f6be03eb0e
vijay <svijay4145@gmail.com>
2024-02-14 17:05:55 +0530

    fixed minor issue

--- server/src/routes/ResourcesRoute.js
+++ server/src/routes/ResourcesRoute.js
@@ -7,7 +7,7 @@ const router = express.Router(); // New router instance from express library
 //routes
 // router.get("/gcr/:branch/:semester", resourceController.getGCRLink);
 // router.post('/addgcr/:branch/:semester', resourceController.addGCRLink);
-// router.post('/get_experiment_url', resourceController.getExperimentUrl);
+router.post('/get_experiment_url', resourceController.getExperimentUrl);
 // router.post('/get_experiment_url_normal', resourceController.getExperimentUrlNormal);
 
 router.get('/get_assignment_experiment_list', resourceController.getBranchSemesterSubjectListAvailable);
